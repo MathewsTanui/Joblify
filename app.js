21a1e9ccb9288e40f19e857b49b9422498933927
MathewsTanui <tanuimathews02@gmail.com>
2024-04-10 14:22:26 +0300

    select the form element and display job listing

---
+++ app.js
@@ -0,0 +1,5 @@
+// Select the form element
+const form = document.getElementById('job-filter-form');
+
+// Select the element to display job listings
+const jobListings = document.getElementById('job-listings');
